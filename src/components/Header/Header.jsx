4c91c78a8d2284c521ba7eb89b18aaec8b6c6c16
ferentsoleh26 <oleh.ferents@interexy.com>
2022-12-18 10:08:35 +0200

    Refactor multiple headers on different routes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,51 +5,106 @@ import { useGetCurrentUserQuery } from '../../services/userApi'
 import SearchIcon from '@mui/icons-material/Search'
 import { Search, SearchIconWrapper, StyledInputBase } from './styles'
 
+const UserInfo = () => {
+  const { data: user } = useGetCurrentUserQuery()
+  return (
+    <Box sx={{
+      height: '86px',
+      width: '200px',
+      display: 'flex',
+      justifyContent: 'center',
+      flexDirection: 'column',
+      paddingLeft: '49px',
+    }}>
+      <Typography fontWeight="bold">{user?.firstName} {user?.lastName}</Typography>
+      <Typography fontWeight="500">{user?.role}</Typography>
+    </Box>
+  )
+}
+
+const ProjectsHeader = ({ sortBy, handleSortBy }) => {
+  return (
+    <>
+      <UserInfo />
+      <Search>
+        <SearchIconWrapper>
+          <SearchIcon/>
+        </SearchIconWrapper>
+        <StyledInputBase
+          placeholder="Знайти проект"
+          inputProps={{ 'aria-label': 'search' }}
+        />
+      </Search>
+      <FormControl variant="standard">
+        <InputLabel id="sort-by-label">Сортувати за</InputLabel>
+        <Select
+          labelId="sort-by-label"
+          id="sort-by"
+          value={sortBy}
+          onChange={handleSortBy}
+          sx={{ width: '204px', height: '42px', }}
+        >
+          <MenuItem value="date">Дата</MenuItem>
+          <MenuItem value="priority">Пріоритет</MenuItem>
+        </Select>
+      </FormControl>
+    </>
+  )
+}
+
+const EmployeeHeader = () => {
+  return (
+    <>
+      <UserInfo />
+      <Box>
+        <Typography fontWeight="600" fontSize="40px" lineHeight="20px" color="#795EA4">Працівники</Typography>
+      </Box>
+      <Box></Box>
+    </>
+  )
+}
+
+const SettingsHeader = () => {
+  return (
+    <>
+      <UserInfo />
+      <Box>
+        <Typography fontWeight="600" fontSize="40px" lineHeight="20px" color="#795EA4">Налаштування</Typography>
+      </Box>
+      <Box></Box>
+    </>
+  )
+}
+
 const Header = () => {
   const [sortBy, setSortBy] = useState('date')
-  const { data: user } = useGetCurrentUserQuery()
+  const [content, setContent] = useState(<></>)
   const { pathname } = useLocation()
 
   const handleSortBy = (e) => setSortBy(e.target.value)
 
+  useEffect(() => {
+    switch (pathname) {
+      case '/dashboard/projects':
+        setContent(<ProjectsHeader sortBy={sortBy} handleSortBy={handleSortBy} />)
+        break
+      case '/dashboard/employee':
+        setContent(<EmployeeHeader />)
+        break;
+      case '/dashboard/settings':
+        setContent(<SettingsHeader />)
+        break;
+      default:
+        setContent(<></>)
+        break
+    }
+  }, [pathname])
+
   return (
     <Box>
       <AppBar color="secondary" position="sticky">
-        <Toolbar disableGutters={true} sx={{ justifyContent: 'space-between', paddingRight: '36px' }}>
-          <Box sx={{
-            height: '86px',
-            width: '200px',
-            display: 'flex',
-            justifyContent: 'center',
-            flexDirection: 'column',
-            paddingLeft: '49px',
-          }}>
-            <Typography fontWeight="bold">{user?.firstName} {user?.lastName}</Typography>
-            <Typography fontWeight="500">{user?.role}</Typography>
-          </Box>
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon/>
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Знайти проект"
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </Search>
-          <FormControl variant="standard">
-            <InputLabel id="sort-by-label">Сортувати за</InputLabel>
-            <Select
-              labelId="sort-by-label"
-              id="sort-by"
-              // label="Сортувати за"
-              value={sortBy}
-              onChange={handleSortBy}
-              sx={{ width: '204px', height: '42px', }}
-            >
-              <MenuItem value="date">Дата</MenuItem>
-              <MenuItem value="priority">Пріоритет</MenuItem>
-            </Select>
-          </FormControl>
+        <Toolbar disableGutters={true} sx={{ justifyContent: 'space-between', paddingRight: '36px', height: '86px' }}>
+          {content}
         </Toolbar>
       </AppBar>
     </Box>
